e4ee814784dbce4df30e2f054dcaacad497b85eb
Fahad Alrasheed <fahad.alrasheed@gmail.com>
2021-02-17 16:29:17 +0200

    EUR USD converter

--- script.js
+++ script.js
@@ -6,31 +6,31 @@
 
 // Data
 const account1 = {
-	owner: "Jonas Schmedtmann",
-	movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-	interestRate: 1.2, // %
-	pin: 1111,
+  owner: "Jonas Schmedtmann",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
 };
 
 const account2 = {
-	owner: "Jessica Davis",
-	movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-	interestRate: 1.5,
-	pin: 2222,
+  owner: "Jessica Davis",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
 };
 
 const account3 = {
-	owner: "Steven Thomas Williams",
-	movements: [200, -200, 340, -300, -20, 50, 400, -460],
-	interestRate: 0.7,
-	pin: 3333,
+  owner: "Steven Thomas Williams",
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
 };
 
 const account4 = {
-	owner: "Sarah Smith",
-	movements: [430, 1000, 700, 50, 90],
-	interestRate: 1,
-	pin: 4444,
+  owner: "Sarah Smith",
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -62,18 +62,20 @@ const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
 const displayMovements = function (movements) {
-	movements.forEach(function (mov, i) {
-		const type = mov > 0 ? "deposit" : "withdrawal";
-		const html = `
+  containerMovements.innerHTML = "";
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+    const html = `
           <div class="movements__row">
           <div class="movements__type movements__type--${type}">${
-			i + 1
-		} ${type}</div>
+      i + 1
+    } ${type}</div>
           <div class="movements__date">3 days ago</div>
           <div class="movements__value">${mov}</div>
         </div>`;
-		containerMovements.insertAdjacentElement("");
-	});
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
 };
 
 displayMovements(account1.movements);
@@ -89,7 +91,31 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);*/
 
-//const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUSD = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUSD;
+// });
+
+const movementsUSD = movements.map((mov) => mov * eurToUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) {
+  movementsUSDfor.push(mov * eurToUSD);
+}
+
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov < 0 ? "withrew" : "deposited"} ${Math.abs(
+      mov
+    )}`
+);
+
+console.log(movementsDescriptions);
 
 /////////////////////////////////////////////////
 /*
