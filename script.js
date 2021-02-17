7df9ea23ac6cc1a96829f67ea5ecbc5d5b8ef159
Fahad Alrasheed <fahad.alrasheed@gmail.com>
2021-02-17 16:57:48 +0200

    computing usernames

--- script.js
+++ script.js
@@ -80,6 +80,19 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(" ")
+      .map((name) => name[0])
+      .join("");
+  });
+};
+
+createUsernames(accounts); //username STW
+console.log(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
